9c58a545af5e60bede46952d619a77ba6b55204d
Saksham Goyal <saksham069@gmail.com>
2022-10-20 01:20:45 +0530

    Dashboard side nav done

--- src/admin/pages/Orders.jsx
+++ src/admin/pages/Orders.jsx
@@ -90,7 +90,7 @@ export default function Orders() {
         </TableBody>
       </Table>
       <div className={classes.seeMore}>
-        <Link color="primary" href="javascript:;">
+        <Link color="primary" >
           See more orders
         </Link>
       </div>
